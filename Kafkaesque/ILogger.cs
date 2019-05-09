060c3355be8a471f98b1cff74486f192f0fe61d3
Mogens Heller Grabe <mookid8000@gmail.com>
2019-05-09 19:47:17 +0200

    add comments

--- Kafkaesque/ILogger.cs
+++ Kafkaesque/ILogger.cs
@@ -2,18 +2,49 @@
 
 namespace Kafkaesque
 {
+    /// <summary>
+    /// Implement to catch logs from Kafkaesque
+    /// </summary>
     public interface ILogger
     {
+        /// <summary>
+        /// Logs at the most verbose level
+        /// </summary>
         void Verbose(string message);
+
+        /// <summary>
+        /// Logs at the most verbose level
+        /// </summary>
         void Verbose(Exception exception, string message);
 
+        /// <summary>
+        /// Logs information
+        /// </summary>
         void Information(string message);
+
+        /// <summary>
+        /// Logs information
+        /// </summary>
         void Information(Exception exception, string message);
         
+        /// <summary>
+        /// Logs a warning
+        /// </summary>
         void Warning(string message);
+
+        /// <summary>
+        /// Logs a warning
+        /// </summary>
         void Warning(Exception exception, string message);
      
+        /// <summary>
+        /// Logs an error
+        /// </summary>
         void Error(string message);
+
+        /// <summary>
+        /// Logs an error
+        /// </summary>
         void Error(Exception exception, string message);
     }
 }
