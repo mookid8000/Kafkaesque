da4397b20df2b690f37c6a13556394e4731fc2fd
Mogens Heller Grabe <mookid8000@gmail.com>
2020-05-11 07:00:23 +0200

    add these tests

--- Kafkaesque.Tests/TestResumptionThoroughly.cs
+++ Kafkaesque.Tests/TestResumptionThoroughly.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,6 +47,54 @@ namespace Kafkaesque.Tests
                 fileNumber = eventData.FileNumber;
                 bytePosition = eventData.BytePosition;
             }
+        }
+
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(100000)]
+        public async Task CheckResumption_Eof(int count)
+        {
+            var directoryPath = GetLogDirectoryPath();
+            var logDirectory = new LogDirectory(directoryPath, new Settings(approximateMaximumFileLength: 4096, numberOfFilesToKeep: int.MaxValue));
+
+            using (var writer = logDirectory.GetWriter())
+            {
+                var data = Enumerable.Range(0, count)
+                    .Select(n => $"THIS IS LINE NUMBER {n}")
+                    .Select(Encoding.UTF8.GetBytes);
+
+                await writer.WriteManyAsync(data);
+            }
+
+            new DirectoryInfo(directoryPath).DumpDirectoryContentsToConsole();
+
+            // read events in a very inefficient way, checking that we can resume at each single line
+            var fileNumber = -1;
+            var bytePosition = -1;
+
+            var linesRead = 0;
+
+            for (var counter = 0; counter < count; counter++)
+            {
+                var expectedText = $"THIS IS LINE NUMBER {counter}";
+                var data = logDirectory.GetReader().ReadEof(fileNumber, bytePosition).FirstOrDefault();
+
+                if (data == LogReader.EOF) break;
+
+                if (!(data is LogEvent eventData)) continue;
+
+                var actualText = Encoding.UTF8.GetString(eventData.Data);
+
+                Assert.That(actualText, Is.EqualTo(expectedText));
+
+                fileNumber = eventData.FileNumber;
+                bytePosition = eventData.BytePosition;
+
+                linesRead++;
+            }
+
+            Assert.That(linesRead, Is.EqualTo(count));
         }
     }
 }
